1a3f2887fcbc10ff3dd27473448dcd1e28e2a727
Joaovvb <rm98938@fiap.com.br>
2024-08-18 22:10:07 -0300

    feat: products teste de api

---
+++ capputeeno/src/components/Produtos/ProductCard.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface ProductCardProps{
+    image: string,
+    title: string,
+    price: number
+}
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    background:rgba(255,255,255,0.4);
+    backdrop-filter:blur(10px);
+    border-radius: 0px 0px 4px 4px;
+
+    width: 256px;
+
+    img{
+        width: 256px;
+        height:300px;
+    }
+
+    h3{
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 150%;
+        color:var(--text-dark-2)
+    }
+
+    p{
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 150%;
+        color: var(---shapes-dark);
+    }
+
+    > div{
+        width: 228px;
+        height: 1px;
+        margin: 8px 0;
+        background: var(--shapes);
+    }
+`
+
+const ProductCard = (props : ProductCardProps) => {
+  return (
+    <CardContainer>
+        <img
+        src={props.image}
+        alt='Imagem do Produto'
+        />
+        <h3>{props.title}</h3>
+        <p>{props.price}</p>
+    </CardContainer>
+)
+}
+
+export default ProductCard
--- capputeeno/src/components/Produtos/ProductsList.tsx
+++ capputeeno/src/components/Produtos/ProductsList.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useProducts } from '@/hooks/useProdutcs'
 import React from 'react'
+import ProductCard from './ProductCard';
 
 interface ProductsListProps{
 
@@ -12,7 +13,15 @@ const ProductsList = (props : ProductsListProps) => {
     console.log(data)
    return (
     <>
-
+        <div>
+            {data?.map(product => 
+            <ProductCard
+            key={product.id}
+            title={product.name}
+            image={product.image_url}
+            price={product.price_in_cents}
+            />)}
+        </div>
     </>
 )
 }
